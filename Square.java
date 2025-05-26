b5b37b2aae4d3229e7d26e95221ebebbde06c519
steel2002 <steelselvin2002@gmail.com>
2025-05-26 15:11:19 +0530

    first commit

---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square {
+    public static void main (String []args){
+
+        int a=6;
+
+        int sum= 4*a;
+        System.out.println("Input value "  +a);
+        System.out.println("Squarecircum value  " + sum);
+    }
+}
