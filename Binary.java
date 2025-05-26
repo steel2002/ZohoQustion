cb7836bc807c86c276d4e5081eaeb2b870e821a5
steel2002 <steelselvin2002@gmail.com>
2025-05-26 15:01:43 +0530

    first commit

--- Binary.java
+++ Binary.java
@@ -13,6 +13,9 @@ public class Binary {
 
         System.out.print("Octal: ");
         convert(num, 8);
+        
+        System.out.print("Hexadecimal: ");
+        convertHex(num);
     }
 
     public static void convert(int num, int base) {
@@ -22,6 +25,23 @@ public class Binary {
         while (num > 0) {
             result[index++] = num % base;
             num = num / base;
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            System.out.print(result[i]);
+        }
+        System.out.println();
+    }
+
+    public static void convertHex(int num) {
+        char[] hexChars = {'0','1','2','3','4','5','6','7','8','9',
+                           'A','B','C','D','E','F'};
+        char[] result = new char[32];
+        int index = 0;
+
+        while (num > 0) {
+            result[index++] = hexChars[num % 16];
+            num = num / 16;
         }
 
         for (int i = index - 1; i >= 0; i--) {
