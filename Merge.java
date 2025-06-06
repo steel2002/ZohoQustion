dc7967a0c4fd58dd8807ce7b593347c03a3f5839
steel2002 <steelselvin2002@gmail.com>
2025-06-06 15:37:38 +0530

    first commit

--- Merge.java
+++ Merge.java
@@ -233,5 +233,24 @@ class frequency {
 
     }
 }
+// 37. Java Program to reverse the elements of an array
+class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.println("Reversed array: " + Arrays.toString(arr));
+    }
+}
+
 
 
